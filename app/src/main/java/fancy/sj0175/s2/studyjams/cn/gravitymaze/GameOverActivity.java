674450797913b445d9c7b5d90b9c3a608da84bde
rushzhou <rushzhou@163.com>
2017-06-11 18:04:03 +0800

    version 1.0 finished

--- app/src/main/java/fancy/sj0175/s2/studyjams/cn/gravitymaze/GameOverActivity.java
+++ app/src/main/java/fancy/sj0175/s2/studyjams/cn/gravitymaze/GameOverActivity.java
@@ -1,7 +1,10 @@
 package fancy.sj0175.s2.studyjams.cn.gravitymaze;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
 
 public class GameOverActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class GameOverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+    }
+
+    public void onStartGame(View view) {
+        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+        finish();
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN){
+            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+            finish();
+            startActivity(intent);
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
---
+++ app/src/main/java/fancy/sj0175/s2/studyjams/cn/gravitymaze/Point.java
@@ -0,0 +1,12 @@
+package fancy.sj0175.s2.studyjams.cn.gravitymaze;
+
+
+public class Point {
+    public int x;
+    public int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
